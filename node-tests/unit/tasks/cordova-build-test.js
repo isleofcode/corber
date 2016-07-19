2d842950d598b50ba0d1cadb178302c479115ae3
@anulman <anulman@gmail.com>
2016-07-19 10:49:08 -0400

    refactor(cdv-build-test): update tests to use instance invocation & minimize td reliance

--- node-tests/unit/tasks/cordova-build-test.js
+++ node-tests/unit/tasks/cordova-build-test.js
@@ -1,40 +1,88 @@
 'use strict';
 
 const td            = require('testdouble');
-const BashTask      = require('../../../lib/tasks/bash');
-const CdvBuildTask  = require('../../../lib/tasks/cordova-build');
+const expect        = require('../../helpers/expect');
 
 const mockProject   = require('../../fixtures/ember-cordova-mock/project');
 const isObject      = td.matchers.isA(Object);
 
 describe('Cordova Build Task', () => {
-  let bashDouble, build;
+  let commandString, buildTask;
 
   beforeEach(() => {
-    bashDouble = td.replace(BashTask.prototype, 'runCommand');
-
-    build = new CdvBuildTask(mockProject);
+    mockBashTask();
   });
 
   afterEach(() => {
     td.reset();
   });
 
-  it('generates a cordova build command', () => {
-    build.run('ios');
+  function runBuild() {
+    buildTask.run();
+  }
 
-    td.verify(bashDouble('cordova build ios', isObject));
+  function mockBashTask() {
+    td.replace('../../../lib/tasks/bash', function(opts) {
+      commandString = opts.command;
+
+      this.run = function() {};
+    });
+  }
+
+  function mockCdvBuildTask(_options) {
+    let CdvBuildTask = require('../../../lib/tasks/cordova-build'),
+        options = defaults(_options, { project: mockProject.project });
+
+    return new CdvBuildTask(options);
+  }
+
+  context('when project is undefined', () => {
+    beforeEach(() => {
+      let options = { project: undefined };
+      buildTask = mockCdvBuildTask(options);
+    });
+
+    it('throws an error', () => {
+      expect(runBuild).to.throw;
+    });
   });
 
-  it('sets platform', () => {
-    build.run('android');
+  context('when platform is ios', () => {
+    beforeEach(() => {
+      let options = { platform: 'ios' };
+      buildTask = mockCdvBuildTask(options);
 
-    td.verify(bashDouble('cordova build android', isObject));
+      runBuild();
+    });
+
+    it('generates a cordova build command', () => {
+      expect(commandString).to.equal('cordova build ios');
+    });
   });
 
-  it('sets release tag for production builds', () => {
-    build.run('ios', 'production');
+  context('when platform is android', () => {
+    beforeEach(() => {
+      let options = { platform: 'android' };
+      buildTask = mockCdvBuildTask(options);
 
-    td.verify(bashDouble('cordova build ios --release', isObject));
+      runBuild();
+    });
+
+    it('sets platform correctly', () => {
+      expect(commandString).to.equal('cordova build android');
+    });
+  });
+
+  context('when release tag is passed', () => {
+    beforeEach(() => {
+      let options = { isRelease: true };
+      buildTask = mockCdvBuildTask(options);
+
+      runBuild();
+    });
+
+    it('sets release tag when passed', () => {
+      expect(commandString).to.equal('cordova build ios --release');
+    });
   });
 });
