20961cb83b56e9def20371bb34eea851a20d49a5
@anulman <anulman@gmail.com>
2017-03-22 18:25:22 -0400

    fix(verbose-logging): update tests to better support cordova + verbose log params

--- node-tests/unit/tasks/cordova-raw-test.js
+++ node-tests/unit/tasks/cordova-raw-test.js
@@ -7,35 +7,22 @@ var cordovaPath     = require('../../../lib/utils/cordova-path');
 var mockProject     = require('../../fixtures/ember-cordova-mock/project');
 var Promise         = require('ember-cli/lib/ext/promise');
 var cordovaLib      = require('cordova-lib');
-var cordovaProj     = cordovaLib.cordova;
-var events          = cordovaLib.events;
 var cordovaLogger   = require('cordova-common').CordovaLogger.get();
 
 describe('Cordova Raw Task', function() {
-  var setupTask = function() {
-    return new RawTask({
+  var rawTask;
+
+  beforeEach(function() {
+    rawTask = new RawTask({
       rawApi: 'platform',
       project: mockProject.project
     });
-  };
-
-  afterEach(function() {
-    td.reset();
   });
 
-  it('sets up cdv logging and attempts a raw cordova call', function(done) {
-    // n.b. need these together & at the top of file due to de-duplication of
-    // `cordovaLogger.subscribe` calls on require
-    td.replace(cordovaLogger, 'subscribe');
-    td.replace(cordovaProj.raw, 'platform', function() {
-      done();
-    });
-
-    var raw = setupTask();
-
-    return raw.run().then(function() {
-      td.verify(cordovaLogger.subscribe(events));
-    });
+  afterEach(function() {
+    rawTask = null;
+    RawTask.isSubscribedToLogs = false;
+    td.reset();
   });
 
   describe('with a mock function', function() {
@@ -51,19 +38,17 @@ describe('Cordova Raw Task', function() {
 
     it('changes to cordova dir', function() {
       var cdvPath = cordovaPath(mockProject.project);
-      var raw = setupTask();
 
-      return raw.run().then(function() {
+      return rawTask.run().then(function() {
         td.verify(chdirDouble(cdvPath));
       });
     });
 
     it('changes back to ember dir on compvarion', function() {
       var emberPath = process.cwd();
-      var raw = setupTask();
 
       return expect(
-        raw.run().then(function() {
+        rawTask.run().then(function() {
           var args = td.explain(chdirDouble).calls[1].args[0];
           return args
         })
@@ -71,49 +56,62 @@ describe('Cordova Raw Task', function() {
     });
 
     describe('verbosity', function() {
-      var raw;
-      var setDebug = function setDebug(term) {
-        var cachedDebug = process.env.DEBUG;
+      ['cordova', '*'].forEach(function(debugGroup) {
+        context('when env DEBUG = ' + debugGroup, function() {
+          var cachedDebug;
 
-        before(function() {
-          process.env.DEBUG = term;
-        });
-
-        after(function() {
-          process.env.DEBUG = cachedDebug;
-        });
-      }
-
-      beforeEach(function() {
-        td.replace(cordovaLogger, 'setLevel');
-        raw = setupTask();
-      });
-
-      context('when env DEBUG = cordova', function() {
-        setDebug('cordova');
-
-        it('logs verbosely', function() {
-          return raw.run().then(function() {
-            td.verify(cordovaLogger.setLevel('verbose'));
+          before(function() {
+            cachedDebug = process.env.DEBUG;
+            process.env.DEBUG = debugGroup;
           });
-        });
-      });
 
-      context('when env DEBUG = *', function() {
-        setDebug('*');
+          after(function() {
+            process.env.DEBUG = cachedDebug;
+          });
 
-        it('logs verbosely', function() {
-          return raw.run().then(function() {
-            td.verify(cordovaLogger.setLevel('verbose'));
+          beforeEach(function() {
+            td.replace(cordovaLogger, 'setLevel');
+            td.replace(cordovaLogger, 'subscribe');
+          });
+
+          context('and DEBUG_LEVEL !== trace', function() {
+            it('sets up cdv logging', function() {
+              return rawTask.run().then(function() {
+                td.verify(cordovaLogger.subscribe(cordovaLib.events));
+              });
+            });
+          });
+
+          context('and DEBUG_LEVEL === trace', function() {
+            var cachedDebugLevel;
+
+            before(function() {
+              cachedDebugLevel = process.env.DEBUG_LEVEL;
+              process.env.DEBUG_LEVEL = 'trace';
+            });
+
+            after(function() {
+              process.env.DEBUG_LEVEL = cachedDebugLevel;
+            });
+
+            it('sets up cdv logging', function() {
+              return rawTask.run().then(function() {
+                td.verify(cordovaLogger.subscribe(cordovaLib.events));
+              });
+            });
+
+            it('logs verbosely', function() {
+              return rawTask.run().then(function() {
+                td.verify(cordovaLogger.setLevel('verbose'));
+              });
+            });
           });
         });
       });
 
       context('when env DEBUG = undefined', function() {
-        setDebug(undefined);
-
         it('does not log verbosely', function() {
-          return raw.run().then(function() {
+          return rawTask.run().then(function() {
             td.verify(cordovaLogger.setLevel(), {
               times: 0,
               ignoreExtraArgs: true
@@ -125,6 +123,17 @@ describe('Cordova Raw Task', function() {
   });
 
   describe('when the raw task fails', function() {
+    var cachedDebug;
+
+    before(function() {
+      cachedDebug = process.env.DEBUG;
+      process.env.DEBUG = undefined;
+    });
+
+    after(function() {
+      process.env.DEBUG = cachedDebug;
+    });
+
     beforeEach(function() {
       td.replace(RawTask.prototype, 'cordovaRawPromise', function() {
         return Promise.reject(new Error('fail'));
@@ -132,9 +141,7 @@ describe('Cordova Raw Task', function() {
     });
 
     it('rejects run() with the failure', function() {
-      var raw = setupTask();
-
-      return expect(raw.run()).to.be.rejectedWith(
+      return expect(rawTask.run()).to.be.rejectedWith(
         /fail/
       );
     });
